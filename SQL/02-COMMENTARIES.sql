86124f4be18a3994df28c544f295eb69a81b82e0
César Sánchez <banerxd783@hotmail.com>
2026-06-19 14:40:45 -0500

    Added '02-COMENTARIES.sql' and renamed 'SELECT.sql' TO '01-SELECT.sql'

---
+++ SQL/02-COMMENTARIES.sql
@@ -0,0 +1,16 @@
+-- COMENTARIES
+/*
+    Commentaries are used to explain the code and make it more readable. They are ignored by the SQL engine and do not affect the execution of the code.
+
+    There are two types of commentaries in SQL:
+
+    1. Single-line comments: These comments start with two dashes (--) and continue until the end of the line.
+    2. Multi-line comments: These comments start with a slash and an asterisk (/*) 
+    and end with an asterisk and a slash (*/). They can span multiple lines.
+
+*/
+
+-- Example
+
+SELECT * FROM products; -- SELECT all columns from the products table
+
